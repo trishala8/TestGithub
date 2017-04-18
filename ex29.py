82315ad6a582853bfaf52a29f47a55f45353cc37
trishala8 <trish2talk@gmail.com>
2017-04-17 22:09:01 -0600

    Add files via upload

---
+++ ex29.py
@@ -0,0 +1,29 @@
+people = 28  # 20
+cats = 13  #30
+dogs = 10   #15   the condition changes
+
+
+if people < cats:
+    print "Too many cats! The world is doomed!"
+
+if people > cats:
+    print "Not many cats! The world is saved!"
+
+if people < dogs:
+    print "The world is drooled on!"
+
+if people > dogs:
+    print "The world is dry!"
+
+
+dogs *= 5   # x = x +1
+
+if people >= dogs:
+    print "People are greater than or equal to dogs."
+
+if people <= dogs:
+    print "People are less than or equal to dogs."
+
+
+if people == dogs:
+    print "People are dogs."
---
+++ ex30.py
@@ -0,0 +1,23 @@
+people = 10
+cars = 90
+trucks = 52
+
+
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We can't decide."
+
+if trucks > cars:      #condition true is executed elif the next condition is executes
+    print "That's too many trucks."
+elif trucks < cars:
+    print "Maybe we could take the trucks."
+else:
+    print "We still can't decide."
+
+if people > trucks:
+    print "Alright, let's just take the trucks."
+else:
+    print "Fine, let's stay home then."
